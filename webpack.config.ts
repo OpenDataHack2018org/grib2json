e1b3d03e2edf7faa160cf5cb148e6cb5d07ba598
Daniel Jabry <daniel.jabry@rezonence.com>
2018-06-09 01:14:42 +0100

    Updated to ignore serverless folder for docker

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,17 @@
 import * as slsw from "serverless-webpack";
 import * as CopyWebpackPlugin from "copy-webpack-plugin";
 import {environment} from "./environment";
+import {join} from "path";
 module.exports = {
     entry: slsw.lib.entries,
     target: "node",
     resolve: {
         extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+    },
+    output: {
+        libraryTarget: "commonjs",
+        path: join(__dirname, ".webpack"),
+        filename: "[name].js"
     },
     module: {
         rules: [
